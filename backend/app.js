fe802ef41191648c77dc7c8970a74233f1eeeb32
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-05-04 13:23:01 +0200

    Socket.io - backend to frontend
    Co-Authored-By: nikolajnowicki <114876157+nikolajnowicki@users.noreply.github.com>
    Co-Authored-By: Filippa Söderlund <114928332+Filippasoderlund@users.noreply.github.com>
    Co-Authored-By: Camilla <114933424+c-ridesjo@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,20 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+
+    cors: {
+
+        origin: 'http://localhost:5173',
+
+        methods: ['GET', 'POST']
+
+    }
+
+});
+
+
 
 app.use(cors());
 app.use(logger("dev"));
@@ -22,4 +36,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-module.exports = app;
+
+
+io.on("connection", function (socket) {
+
+    console.log("user connected")
+
+
+    socket.on("disconnect", function () {
+        console.log("user disconected")
+    })
+
+    socket.on("chat message", function (msg) {
+        console.log("msg", msg)
+        io.emit("chat message", msg);
+    })
+
+})
+
+
+module.exports = { app: app, server: server };
--- backend/conn.js
+++ backend/conn.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+/*const mysql = require("mysql2");
 require("dotenv").config();
 
 connection = mysql.createConnection({
@@ -10,3 +10,4 @@ connection = mysql.createConnection({
 });
 
 module.exports = connection;
+*/
