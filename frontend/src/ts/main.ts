c8de57667c448a585927068f30fc7e89d3795325
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-05-05 14:05:44 +0200

    Task - implementation
    Added css, javascript and socket.
    
    Works like a chat ATM

--- frontend/src/ts/main.ts
+++ frontend/src/ts/main.ts
@@ -4,7 +4,36 @@ import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 
 export const socket = io("http://localhost:3000");
 import { renderHeader } from "./header";
+import { renderTasks } from "./tasks";
+export { io };
+
+
+
+
+socket.on("connect", () => {
+    console.log("frontend");
+});
+
+socket.on("socket connected", (socketId: string) => {
+    console.log("socket connected", socketId);
+    const socketIdElement = document.getElementById("socket-id");
+    if (socketIdElement) {
+        socketIdElement.innerText = socketId;
+
+    }
+
+});
+
+
+
 
 renderHeader();
+renderTasks();
+
+
+
+
+
+
 
 
